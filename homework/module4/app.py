210427717a5cfd37b45f35acd9aeac161e882a9b
danil-prekin <152359981+danil-prekin@users.noreply.github.com>
2026-03-29 13:52:06 +0500

    Upload app.py

--- homework/module4/app.py
+++ homework/module4/app.py
@@ -2,7 +2,7 @@ import os
 import shlex
 import subprocess
 from flask import Flask, request, render_template_string
-from .forms import RegistrationForm
+from forms import RegistrationForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key-here'
@@ -42,7 +42,6 @@ def registration():
         return f'Registration successful for {form.name.data}!'
     return render_template_string(FORM_TEMPLATE, form=form, errors=form.errors)
 
-
 @app.route('/uptime')
 def uptime():
     try:
@@ -51,7 +50,6 @@ def uptime():
         return f'Current uptime is {uptime_str}'
     except subprocess.CalledProcessError:
         return 'Error getting uptime', 500
-
 
 @app.route('/ps')
 def ps_endpoint():
@@ -66,7 +64,6 @@ def ps_endpoint():
     except subprocess.CalledProcessError as e:
         output = e.stderr or str(e)
     return f'<pre>{output}</pre>'
-
 
 if __name__ == '__main__':
     app.run(debug=True)
